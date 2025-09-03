ed5fbf5973b366128cb73e3981e64dbd3d1eb298
ruudkuiperi <ruud.kuiperi@gmail.com>
2025-09-03 12:13:43 +0200

    make restart more efficient

--- bikeshare.py
+++ bikeshare.py
@@ -227,8 +227,8 @@ def main():
         user_stats(df)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        restart = input('\nWould you like to restart? Enter y or n.\n')
+        if restart.lower() != 'y':
             break
 
 
